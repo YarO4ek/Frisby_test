d606f8a8e941015ff729db27517c81867dff2775
Yaroslav Maslov <yaroslavmaslov0951@gmail.com>
2023-07-06 14:38:51 +0200

    .

--- tests/day_14.js
+++ tests/day_14.js
@@ -6,7 +6,7 @@ describe("Day 14", () => {
         const response = await frisby
             .fetch("https://api.github.com/user/repos", {
                 headers: {
-                    Authorization: "Bearer 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" ,
+                    Authorization: "Bearer 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" ,
                 },
             })
             .expect("status", 200);
