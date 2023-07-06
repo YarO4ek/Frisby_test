f62542e9abfb868e9016e1a9af3a6845f58e857c
Yaroslav Maslov <yaroslavmaslov0951@gmail.com>
2023-07-06 15:55:32 +0200

    Update day_14.js

--- __tests__/day_14.js
+++ __tests__/day_14.js
@@ -1,12 +1,12 @@
 const frisby = require("frisby");
-
+const token = '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';
 
 describe("Day 14", () => {
     it("Get github repositories", async () => {
         const response = await frisby
             .fetch("https://api.github.com/user/repos", {
                 headers: {
-                    Authorization: "Bearer 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" ,
+                    Authorization: `Bearer ${token}`,
                 },
             })
             .expect("status", 200);
